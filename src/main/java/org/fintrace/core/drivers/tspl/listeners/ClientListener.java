8939b0b2af895ab3ff37042f33c529298d28a59f
Venkaiah Chowdary Koneru <koneru.chowdary@gmail.com>
2018-03-26 20:00:39 +0400

    change copyright header

--- src/main/java/org/fintrace/core/drivers/tspl/listeners/ClientListener.java
+++ src/main/java/org/fintrace/core/drivers/tspl/listeners/ClientListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Finium Solutions
+ * Copyright 2017 fintrace (https://fintrace.org/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
